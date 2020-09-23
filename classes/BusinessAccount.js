906b6279ce4530eb94c78711f31e5115e8efd20f
Janelle Baetiong <jbaetion@my.centennialcollege.ca>
2020-09-23 11:13:33 -0400

    Added the models

---
+++ classes/BusinessAccount.js
@@ -0,0 +1,14 @@
+import CustomersVisited from "./CustomersVisitedLog"
+
+export default class BusinessAccount {
+    businessName = "";
+    addressLine1 = "";
+    addressLine2 = "";
+    city = "";
+    province = "";
+    postalCode = "";
+    password = "";
+    phoneNumber = "";
+    emailAddress = "";
+    customerVisitedLog = [CustomersVisited];
+}
---
+++ classes/CustomersVisitedLog.js
@@ -0,0 +1,7 @@
+import UserAccount from "./UserAccount";
+
+export default class PlacesVisitedLog {
+    userAccount = UserAccount;
+    dateVisited = "";
+    timeVisited = "";
+}
