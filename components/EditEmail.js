e16b43b829ae84bc288ed4f5b638c92be3dbb674
Janelle Baetiong <jbaetion@my.centennialcollege.ca>
2020-09-30 13:07:26 -0400

    Edit Email implemented

--- components/EditEmail.js
+++ components/EditEmail.js
@@ -1,51 +1,72 @@
 import React, { Component, useState } from "react";
-import { StyleSheet, View, Image, SafeAreaView, TextInput } from "react-native";
+import { StyleSheet, View, Image, SafeAreaView, TextInput, Alert } from "react-native";
 import { Divider, Text, RadioButton } from "react-native-paper";
 import HeaderSecond from "./HeaderSecond";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import API from '../api';
 import { NetworkContext } from "../NetworkContext";
-import API from "../api";
 
 const EditEmail = ({ navigation, route }) => {
-  const { email } = route.params;
   const network = React.useContext(NetworkContext);
-
-  const [emailVal, setEmail] = useState({ email });
-
+  const { email } = route.params;
+  
+  const [textInputEmail, setTextInputEmail] = useState(email);
+  
   const onPressSave = () => {
-    if (emailVal.email === "") {
-      alert("Please Enter email");
-      return;
+
+    let emailVal;
+    let userType;
+
+    if (!textInputEmail.trim()) {
+      emailVal = '';
     } else {
-      API.put(
-        `/api/businessAccount/${network.id}/email?newEmail=${emailVal.email}`
-      )
-        .then((res) => {
-          Alert.alert("Success", "Email has been updated.");
-        })
-        .catch((error) => console.log("failed to update"));
+      emailVal = textInputEmail;
     }
+
+    if (network.businessName !== null) {
+      userType = 'businessAccount';
+    } else {
+      userType = 'userAccount';
+    }
+      
+      console.log('what is new email: ', emailVal);
+      console.log('what is network id: ', network.id);
+      console.log('what is userType: ', userType);
+
+
+      API.put("api/" + userType + "/" + network.id + "/email?newEmail=" + emailVal)
+        .then((response) => {
+          Alert.alert(
+            "Success!",
+            "Email updated!",
+
+            [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+            { cancelable: false }
+          );
+          navigation.navigate('BusinessAccount');
+        })
+        .catch((e) => {
+            console.error("error email update " + e);
+            alert("Unable to update email! ");
+        });
+    
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <HeaderSecond
-          title="Email"
-          navigation={navigation}
-          onPress={onPressSave}
-        />
+        <HeaderSecond title="Account" navigation={navigation} onPress={onPressSave} />
         <View style={styles.mainView}>
-          <Text style={styles.textField}>Email </Text>
+          <Text style={styles.textField}>EMAIL</Text>
           <View style={styles.subView}>
-            <TextInput
+            <TextInput 
               style={styles.subTxtField}
-              onChangeText={(value) => setEmail(value)}
-              value={emailVal.email}
+              onChangeText={(value) => setTextInputEmail(value)}
+              value={textInputEmail}
               spellCheck={false}
               autoCorrect={false}
-              maxLength={100}
-            ></TextInput>
+              maxLength={30}
+              ></TextInput>
           </View>
         </View>
       </View>
